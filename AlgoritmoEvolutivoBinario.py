9a5387ca72d8aa9506f1259130c2e335082f60b5
satiros12 <satiros12@gmail.com>
2014-01-22 00:33:16 +0100

    He creado el archivo del algoritmo genetico

---
+++ AlgoritmoEvolutivoBinario.py
@@ -0,0 +1,17 @@
+import Mutation
+import Selection
+class AlgoritmoGenetico:
+ def __init__(self, maxIndividuals = 10, maxNumGenerations = 100, maxQuality = 1.0).
+  self.generacion = None
+  self.numGeneration = 0
+  self.maxIndividuals = maxIndividuos
+  self.maxNumGeneration = maxNumGenerations
+  
+ def sart(self):
+ def mutation(self):
+ def evaluation(self):
+ 
+class Individual:
+ def __init__(self):
+  
+class Generation:
