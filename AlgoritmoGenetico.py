25c7a98299a390fd8ed009620fdeb260171c241e
satiros12 <satiros12@gmail.com>
2014-01-22 12:13:14 +0100

    Se ha retocado el AlgoritmoGenetico, casi nada

--- AlgoritmoGenetico.py
+++ AlgoritmoGenetico.py
@@ -1,17 +1,24 @@
 import Mutation
 import Selection
 class AlgoritmoGenetico:
- def __init__(self, maxIndividuals = 10, maxNumGenerations = 100, maxQuality = 1.0).
+ def __init__(self, maxIndividuals = 10, maxNumGenerations = 100, maxQuality = 1.0):
   self.generacion = None
   self.numGeneration = 0
   self.maxIndividuals = maxIndividuos
   self.maxNumGeneration = maxNumGenerations
   
  def sart(self):
+    pass
  def mutation(self):
+     pass
  def evaluation(self):
+     pass
  
 class Individual:
- def __init__(self):
-  
+  def __init__(self):
+     pass
+
 class Generation:
+  def __init__(self):
+     pass
+   
